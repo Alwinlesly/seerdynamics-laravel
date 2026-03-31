460f08e08781bf80eecd7232ad5f4336c5961f19
Alwin Lesly <alwinlesly98@gmail.com>
2026-04-01 00:01:46 +0530

    footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,8 +4,14 @@
         <img src="{{ asset('assets/img/Business-Applications.webp') }}" alt="Business Applications">
     </div>
     <div class="store-apps">
-        <img src="{{ asset('assets/img/microsoft-store.png') }}" alt="Microsoft Store">
-        <img src="{{ asset('assets/img/playstore.png') }}" alt="Google Play">
-        <img src="{{ asset('assets/img/appstore.png') }}" alt="App Store">
+        <a href="https://appsource.microsoft.com/en-us/marketplace/apps?search=seer%20dynamics%20technologies&page=1" target="_blank" rel="noopener noreferrer">
+            <img src="{{ asset('assets/img/microsoft-store.png') }}" alt="Microsoft Store">
+        </a>
+        <a href="https://play.google.com/store/apps/developer?id=Seer+Dynamics+Technologies" target="_blank" rel="noopener noreferrer">
+            <img src="{{ asset('assets/img/playstore.png') }}" alt="Google Play">
+        </a>
+        <a href="https://apps.apple.com/kh/developer/seerdynamics-consulting-private-limited/id1580792251" target="_blank" rel="noopener noreferrer">
+            <img src="{{ asset('assets/img/appstore.png') }}" alt="App Store">
+        </a>
     </div>
 </div>
